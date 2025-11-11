6b0f85f64aa9facb2e16be4b3f55399677714acc
EliasMez <elias.mezine@laposte.net>
2025-11-11 21:11:49 +0100

    chore(sql): modif lint correction sql

--- dbt_transformations/NYC_Taxi_dbt/models/final/dim_time.sql
+++ dbt_transformations/NYC_Taxi_dbt/models/final/dim_time.sql
@@ -1,16 +1,15 @@
 with times as (
-    select distinct
-        time(tpep_pickup_datetime) as time
+    select distinct time(tpep_pickup_datetime) as trip_time
     from {{ ref('yellow_taxi_trips_stg') }}
-    union
-    select distinct
-        time(tpep_dropoff_datetime) as time
+    union distinct
+    select distinct time(tpep_dropoff_datetime) as trip_time
     from {{ ref('yellow_taxi_trips_stg') }}
 )
+
 select
+    trip_time,
     row_number() over (order by time) as time_key,
-    time,
     extract(hour from time) as hour,
     extract(minute from time) as minute,
-    extract(second from time) as second,
+    extract(second from time) as second
 from times
