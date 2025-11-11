3e4dd1dac7d5ab76d1c099a5138accebb71d8f3e
EliasMez <elias.mezine@laposte.net>
2025-11-11 21:11:49 +0100

    chore(sql): modif lint correction sql

--- dbt_transformations/NYC_Taxi_dbt/models/final/fact_trips.sql
+++ dbt_transformations/NYC_Taxi_dbt/models/final/fact_trips.sql
@@ -7,6 +7,7 @@ with trips as (
         time(tpep_dropoff_datetime) as dropoff_time
     from {{ ref('yellow_taxi_trips_stg') }}
 )
+
 select
     -- Clés étrangères
     dd_pickup.date_key as pickup_date_key,
@@ -24,9 +25,9 @@ select
     trips.passenger_count
 
 from trips
-left join {{ ref('dim_date') }} dd_pickup on trips.pickup_date = dd_pickup.date
-left join {{ ref('dim_time') }} dt_pickup on trips.pickup_time = dt_pickup.time
-left join {{ ref('dim_date') }} dd_dropoff on trips.dropoff_date = dd_dropoff.date
-left join {{ ref('dim_time') }} dt_dropoff on trips.dropoff_time = dt_dropoff.time
-left join {{ ref('dim_locations') }} loc_pickup on trips.pulocationid = loc_pickup.location_id
-left join {{ ref('dim_locations') }} loc_dropoff on trips.dolocationid = loc_dropoff.location_id
+left join {{ ref('dim_date') }} as dd_pickup on trips.pickup_date = dd_pickup.date
+left join {{ ref('dim_time') }} as dt_pickup on trips.pickup_time = dt_pickup.time
+left join {{ ref('dim_date') }} as dd_dropoff on trips.dropoff_date = dd_dropoff.date
+left join {{ ref('dim_time') }} as dt_dropoff on trips.dropoff_time = dt_dropoff.time
+left join {{ ref('dim_locations') }} as loc_pickup on trips.pulocationid = loc_pickup.location_id
+left join {{ ref('dim_locations') }} as loc_dropoff on trips.dolocationid = loc_dropoff.location_id
--- dbt_transformations/NYC_Taxi_dbt/models/marts/zone_analysis.sql
+++ dbt_transformations/NYC_Taxi_dbt/models/marts/zone_analysis.sql
@@ -1,7 +1,7 @@
 select
     pulocationid,
     count(*) as trip_count,
-    round(avg(total_amount),2) as avg_revenue,
+    round(avg(total_amount), 2) as avg_revenue,
     sum(total_amount) as total_revenue
 from {{ ref('int_trip_metrics') }}
 group by pulocationid
