abcea474efafdce680f9b4633a84b43ba53ee254
EliasMez <elias.mezine@laposte.net>
2025-11-22 02:43:41 +0100

    fix(dbt): correction dim time trasnfo

--- dbt_transformations/NYC_Taxi_dbt/models/final/dim_time.sql
+++ dbt_transformations/NYC_Taxi_dbt/models/final/dim_time.sql
@@ -8,8 +8,8 @@ with times as (
 
 select
     trip_time,
-    row_number() over (order by time) as time_key,
-    extract(hour from time) as hour,
-    extract(minute from time) as minute,
-    extract(second from time) as second
+    row_number() over (order by trip_time) as time_key,
+    extract(hour from trip_time) as hour,
+    extract(minute from trip_time) as minute,
+    extract(second from trip_time) as second
 from times
--- dbt_transformations/NYC_Taxi_dbt/models/final/fact_trips.sql
+++ dbt_transformations/NYC_Taxi_dbt/models/final/fact_trips.sql
@@ -9,7 +9,6 @@ with trips as (
 )
 
 select
-    -- Clés étrangères
     dd_pickup.date_key as pickup_date_key,
     dt_pickup.time_key as pickup_time_key,
     dd_dropoff.date_key as dropoff_date_key,
@@ -17,7 +16,6 @@ select
     loc_pickup.location_key as pickup_location_key,
     loc_dropoff.location_key as dropoff_location_key,
 
-    -- Mesures
     trips.trip_distance,
     trips.fare_amount,
     trips.tip_amount,
@@ -25,9 +23,9 @@ select
     trips.passenger_count
 
 from trips
-left join {{ ref('dim_date') }} as dd_pickup on trips.pickup_date = dd_pickup.date
-left join {{ ref('dim_time') }} as dt_pickup on trips.pickup_time = dt_pickup.time
-left join {{ ref('dim_date') }} as dd_dropoff on trips.dropoff_date = dd_dropoff.date
-left join {{ ref('dim_time') }} as dt_dropoff on trips.dropoff_time = dt_dropoff.time
+left join {{ ref('dim_date') }} as dd_pickup on trips.pickup_date = dd_pickup.trip_date
+left join {{ ref('dim_time') }} as dt_pickup on trips.pickup_time = dt_pickup.trip_time
+left join {{ ref('dim_date') }} as dd_dropoff on trips.dropoff_date = dd_dropoff.trip_date
+left join {{ ref('dim_time') }} as dt_dropoff on trips.dropoff_time = dt_dropoff.trip_time
 left join {{ ref('dim_locations') }} as loc_pickup on trips.pulocationid = loc_pickup.location_id
 left join {{ ref('dim_locations') }} as loc_dropoff on trips.dolocationid = loc_dropoff.location_id
