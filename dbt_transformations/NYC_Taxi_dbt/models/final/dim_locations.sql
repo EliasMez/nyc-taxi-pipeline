3b81cd6c3e8ab69e8f08efa90937223da8577225
EliasMez <elias.mezine@laposte.net>
2026-02-22 22:25:18 +0100

    fix(dbt): correction lineage

--- dbt_transformations/NYC_Taxi_dbt/models/final/dim_locations.sql
+++ dbt_transformations/NYC_Taxi_dbt/models/final/dim_locations.sql
@@ -3,4 +3,4 @@ select
     borough::VARCHAR(55) as borough,
     zone::VARCHAR(55) as zone_name,
     service_zone::VARCHAR(55) as service_zone
-from {{ ref('taxi_zone_lookup') }}
+from {{ ref('taxi_zone_stg') }}
